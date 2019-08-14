5531cdaacab37dced3ae917cf13b666030cbce92
Higor Nucci <higornucci@gmail.com>
2019-08-14 15:26:40 -0400

    Escrevendo "SO carregado" na tela de loading

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 Códigos dos trabalhos da disciplina de doutorado de Sistemas Operacionais
 
 1. BootLoader;
+
+>  as -o boot.o boot.s
+> ld -o boot.bin --oformat binary -e _iniciar -Ttext 0x7c00 -o boot.bin boot.o
+> qemu-system-x86_64 boot.bin
+
+
 2. non-preemptive kernel;
 3. preemptive kernel;
 4. interprocess communication and device driver;
--- bootloader/boot.s
+++ bootloader/boot.s
@@ -1,11 +1,20 @@
 .code16 # avisa o assembler que usaremos o modo 16 bit
-.global _start # coloca a variável _start disponível fora do programa
+.global _iniciar # coloca a variável _iniciar disponível fora do programa
 
-_start:
-  mov $0x0e41, %ax # atribui AH para 0xe (função teletype) e al para 0x41 (ASCII "A")
-  int $0x10 # chama a função em ah da interrupção 0x10
+_iniciar:
+  mov $msg, %si # carrega o endereço de $msg para si
+  mov $0xe, %ah   # carrega 0xe (número da função de interrupição 0x10) para ah
+imprimir_char:
+  lodsb # carrega o byte do endereço em si para al e incrementa si
+  cmp $0, %al # compara o conteúdo em AL com zero
+  je finalizar # se al == 0, vá para "finalizar"
+  int $0x10 # imprime o char em al para a tela
+  jmp imprimir_char # move para o próximo byte
+finalizar:
   hlt # para a execução
 
-.fill 510-(.-_start), 1, 0 # adiciona zeros para deixar o arquivo com tamanho de 510 bytes
+msg: .asciz "SO carregado!" # armazena a string (mais um byte com valor "0") e nos da acesso via $msg
+
+.fill 510-(.-_iniciar), 1, 0 # adiciona zeros para deixar o arquivo com tamanho de 510 bytes
 .word 0xaa55 # bytes que avisam a BIOS que esse código é bootável that tell BIOS that this is bootable
 
