86424ea8040a41c3d5a04bd3cc3c35c2d0e0b454
Higor Nucci <higornucci@gmail.com>
2019-08-20 16:48:10 -0400

    criando a imagem_small

--- bootloader/p1/bootblock.s
+++ bootloader/p1/bootblock.s
@@ -1,21 +1,19 @@
-# Author(s): <Your name here>
-# bootloader
-# TODO: Comment on the status of your submission. Largely unimplemented
+# bootblock.s
 
 # .equ symbol, expression
 # These directive set the value of the symbol to the expression
-# memory constants
-.equ  BOOT_SEGMENT,0x7c0
-# more memory constants...
-.equ  KERNEL_SEGMENT,0x100
-	
-# utility constants
-.equ  DISK_READ,0x02
-# more utility constants...
+    .equ    BOOT_SEGMENT,0x07c0
+    .equ    DISPLAY_SEGMENT,0xb800
+    #set the stack point to 0xFFFF
+    #which refers to the memory map at http://wiki.osdev.org/Memory_Map_(x86)
+    .equ    STACK_ADD_SP,0xffff
+    .equ    STACK_ADD_SS,0x0
+    .equ    KERNEL_ADD,0x1000
 
-.text               #Code segment
-.globl    _start    #The entry point must be global
-.code16             #Real mode
+.text               # Code segment
+.globl    _start    # The entry point must be global
+.code16             # Real mode
+
 
 #
 # The first instruction to execute in a program is called the entry
@@ -24,28 +22,106 @@
 #
 
 _start:
-	jmp load_kernel
-
-# Area reserved for createimage to write the OS size
+    jmp     over
 os_size:
-	.word   0
-	.word   0
+    # Area reserved for createimage to write the OS size
+    .word   0
+    .word   0
 
-# setup registers for kernel data and disk read	
-load_kernel:
-	# try to print a character on start
-	movb	$0x0e,%ah
-	movb	$'S',%al
-	movb	$0x00,%bh
-	movb	$0x02,%bl
-	int	$0x10
-	xchg	%bx,%bx	# this is a magic breakpoint
-	
-# setup the kernel stack
-setup_stack:	
+    # This is where the bootloader goes
+over:
+    #set stack
+    movw    $STACK_ADD_SS,%ax
+    movw    %ax,%ss
+    movw    $STACK_ADD_SP,%sp
 
-# switch control to kernel
-switch_to_kernel:
-	
-end_of_boot:
-	nop
+    #print message
+    movw    $BOOTSTRING1, %si 
+    call    print_string
+
+    movw    $BOOTSTRING2, %si 
+    call    print_string
+
+    #load kernel to 0x1000
+    #accordding to the createimage's output,the kernel is at sector 2-10(1 based), the size is 9 sectors
+    #more details about int 0x13: http://en.wikipedia.org/wiki/INT_13H#INT_13h_AH.3D00h:_Reset_Disk_Drive
+
+    #reset disk drive
+    movb    $0x0, %ah
+    #drive number 1st hard disk
+    movb    $0x80, %dl
+    int     $0x13
+
+    #read 9 sectors
+    movb    $0x09, %al
+    #read from 2nd sector
+    movb    $0x02, %cl
+    #head number
+    movb    $0x0,  %dh
+    #drive number
+    movb    $0x80,  %dl
+    #es:bx = pointer where to palce information read from disk
+    movw    $0x0,  %bx
+    movw    %bx,   %es
+    movw    $0x1000,%bx
+    
+    movb    $0x2,%ah
+    int $0x13
+
+    #setup ds
+    movw    $0x0, %ax
+    movw    %ax, %ds
+    #jump to kernel
+    movw    $KERNEL_ADD,%ax
+    jmp     %ax
+
+forever: 
+    # Loop forever
+    hlt
+    jmp     forever
+
+print_string:
+    pushw   %es
+    pushw   %ax
+    pushw   %bx
+    pushw   %cx
+    pushw   %dx
+
+    movw    $DISPLAY_SEGMENT, %bx
+    movw    %bx, %es
+    movw    $BOOT_SEGMENT, %bx
+    movw    %bx, %ds
+
+    #set the color mode
+    movb    $0x1f, %ah
+    xorw    %bx,%bx
+    cld
+
+    
+    lodsb
+    cmpb    $0x0, %al
+    jz      print_end
+print_loop1:
+    #write data to video memory
+    movw    %ax, %es:(%bx)
+    #see detail in reference about lodsb intruction
+    lodsb
+    #every item is 2 byte
+    inc    %bx
+    inc    %bx
+    cmpb    $0x0, %al
+    jnz     print_loop1 
+
+print_end:
+    popw %dx
+    popw %cx
+    popw %bx
+    popw %ax
+    popw %es
+    ret
+
+BOOTSTRING1:
+.asciz "Bootloader Starting\0xA"
+
+BOOTSTRING2:
+.asciz "Kernel Loading\0xA"
--- bootloader/p1/createimage.c
+++ bootloader/p1/createimage.c
@@ -1,97 +1,158 @@
-/* Author(s): <Your name here>
- * Creates operating system image suitable for placement on a boot disk
-*/
-/* TODO: Comment on the status of your submission. Largely unimplemented */
-#include <assert.h>
-#include <elf.h>
-#include <errno.h>
-#include <stdarg.h>
+/* createimage.c -- create a bootable image in 16 real mode from several elf file
+ */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "createimage.h"
 
-#define IMAGE_FILE "./image"
-#define ARGS "[--extended] [--vm] <bootblock> <executable-file> ..."
+int file_process(FILE *elf_file, FILE *image, char *elf_filename);
+long byte_get (unsigned char *field, int size);
 
-#define SECTOR_SIZE 512       /* floppy sector size in bytes */
-#define BOOTLOADER_SIG_OFFSET 0x1fe /* offset for boot loader signature */
-// more defines...
+int main (int argc, char ** argv)
+{
+    //here hasn't check the magic numbers of elf
+    if (argc != 3) {
+        printf("USAGE:%s bootblock kernel\n", argv[0]);
+        return -1;
+    } 
+    FILE *bootblock, *kernel, *image;
+    if ((bootblock = fopen (argv[1], "rb")) == NULL) {
+        printf("can't open %s\n", argv[1]);
+        return -1;
+    }
+    if ((image = fopen ("image", "wb")) == NULL) {
+        printf("can't open image!\n");
+        return -1;
+    }
+    if (file_process(bootblock, image, argv[1])) {
+        printf("process bootblock failed\n");
+        return -1;
+    }
+        
+    if ((kernel = fopen (argv[2], "rb")) == NULL) {
+        printf("can't open %s\n", argv[2]);
+        return -1;
+    }
+    if (file_process(kernel, image, argv[2])) {
+        printf("process kernel failed\n");
+        return -1;
+    }
 
-/* Reads in an executable file in ELF format*/
-Elf32_Phdr * read_exec_file(FILE **execfile, char *filename, Elf32_Ehdr **ehdr){
- 
-  return NULL;
-}
+    fclose(bootblock);
+    fclose(kernel);
+    fclose(image);
 
-/* Writes the bootblock to the image file */
-void write_bootblock(FILE **imagefile,FILE *bootfile,Elf32_Ehdr *boot_header, Elf32_Phdr *boot_phdr){
- 
-
-}
-
-/* Writes the kernel to the image file */
-void write_kernel(FILE **imagefile,FILE *kernelfile,Elf32_Ehdr *kernel_header, Elf32_Phdr *kernel_phdr){
-
- 
-}
-
-/* Counts the number of sectors in the kernel */
-int count_kernel_sectors(Elf32_Ehdr *kernel_header, Elf32_Phdr *kernel_phdr){
-   
     return 0;
 }
 
-/* Records the number of sectors in the kernel */
-void record_kernel_sectors(FILE **imagefile,Elf32_Ehdr *kernel_header, Elf32_Phdr *kernel_phdr, int num_sec){
-    
+long byte_get (unsigned char *field, int size)
+{
+    switch (size)
+    {
+        case 1:
+            return *field;
+
+        case 2:
+            return  ((unsigned int) (field[0])) | (((unsigned int) (field[1])) << 8);
+        case 4:
+            return  ((unsigned long) (field[0]))
+                |    (((unsigned long) (field[1])) << 8)
+                |    (((unsigned long) (field[2])) << 16)
+                |    (((unsigned long) (field[3])) << 24);
+        default:
+            printf("byte_get error\n");
+            return -1;
+    }
 }
 
 
-/* Prints segment information for --extended option */
-void extended_opt(Elf32_Phdr *bph, int k_phnum, Elf32_Phdr *kph, int num_sec){
+/* read information from elf file, and write LOAD segment to image file 
+ *
+ * note: the structure in file is not aligned, we just read it from file byte
+ * by byte
+ */
+int file_process(FILE *elf_file, FILE *image, char *elf_filename)
+{
+    unsigned int header_sz, pheader_sz;
+    unsigned long phoff;
+    unsigned int p_offset;
+    unsigned int p_filesz;
+    unsigned int p_memsz;
+    elf_header header;
+    elf_program_header pheader;
 
-  /* print number of disk sectors used by the image */
+    header_sz = sizeof (elf_header);
+    pheader_sz = sizeof(elf_program_header);
 
-  
-  /*bootblock segment info */
- 
+    printf("processing %s:\n", elf_filename);
+    printf("header size is: %d\n", header_sz);
+    printf("program header size is: %d\n", pheader_sz);
 
-  /* print kernel segment info */
-  
+    if (header_sz != fread(&header, 1, header_sz, elf_file)) {
+        printf("read error!\n");
+        return -1;
+    }
 
-  /* print kernel size in sectors */
+    //get program header's offset
+    phoff = byte_get(header.e_phoff, sizeof(header.e_phoff));
+
+    printf("Program header table offset in file is :\t %u\n", phoff);
+
+    if (fseek(elf_file, phoff, SEEK_SET)) {
+        printf("fseek %s failed! at line %d\n", elf_filename, __LINE__);
+        return -1;
+    }
+    //printf("the current position: %d\n", ftell(elf_file));
+
+    if (pheader_sz != fread(&pheader, 1, pheader_sz, elf_file)) {
+        printf("read error at line %d!\n", __LINE__);
+        return -1;
+    }
+    //get the LOAD segment's offset, filesz, mensz
+    p_offset = byte_get(pheader.p_offset, sizeof(pheader.p_offset));
+    p_filesz = byte_get(pheader.p_filesz, sizeof(pheader.p_filesz));
+    p_memsz = byte_get(pheader.p_memsz, sizeof(pheader.p_memsz));
+    printf("p_offset: 0x%x\tp_filesz: 0x%x\tp_memsz: 0x%x\t\n", p_offset, p_filesz, p_memsz);
+    //write elf's LOAD segment to image, and pad to 512 bytes(1 sector)
+    char *buffer;
+    const unsigned int sector_sz = 512;
+    const char MBR_signature[] = {0x55, 0xaa};
+    unsigned int n_sector;
+    unsigned int n_byte;
+
+    if (p_memsz % sector_sz != 0)
+        n_sector = p_memsz / sector_sz + 1;
+    else
+        n_sector = p_memsz / sector_sz;
+
+    n_byte = n_sector * sector_sz;
+
+    if (!(buffer = (char *)calloc(n_byte, sizeof(char)))) {
+        printf("malloc buffer failed! at line %d\n", __LINE__);
+        return -1;
+    }
+    if (fseek(elf_file, p_offset, SEEK_SET)) {
+        printf("fseek %s failed! at line %d\n", elf_filename, __LINE__);
+        return -1;
+    }
+    if (p_filesz != fread(buffer, 1, p_filesz, elf_file)) {
+        printf("read error at line %d!\n", __LINE__);
+        return -1;
+    }
+    if (n_byte != fwrite(buffer, 1, n_byte, image)) {
+        printf("write error at line %d!\n", __LINE__);
+        return -1;
+    }
+    //write MBR signature to image, which is 2 bytes
+    if (fseek(image, 510, SEEK_SET)) {
+        printf("fseek %s failed! at line %d\n", elf_filename, __LINE__);
+        return -1;
+    }
+    if (2 != fwrite(MBR_signature, 1, 2, image)) {
+        printf("write error at line %d!\n", __LINE__);
+        return -1;
+    }
+
+    printf("write image:\n%d sectors,\t%d bytes\n", n_sector, n_byte);
+     
+    return 0;
 }
-// more helper functions...
-
-/* MAIN */
-// ignore the --vm argument when implementing (project 1)
-int main(int argc, char **argv){
-  FILE *kernelfile, *bootfile,*imagefile;  //file pointers for bootblock,kernel and image
-  Elf32_Ehdr *boot_header = malloc(sizeof(Elf32_Ehdr));//bootblock ELF header
-  Elf32_Ehdr *kernel_header = malloc(sizeof(Elf32_Ehdr));//kernel ELF header
-
-  Elf32_Phdr *boot_program_header; //bootblock ELF program header
-  Elf32_Phdr *kernel_program_header; //kernel ELF program header
-
-  /* create image file */
-
-  /* read executable bootblock file */  
-
-  /* write bootblock */  
-
-  /* read executable kernel file */
-
-  /* write kernel segments to image */
-
-  /* tell the bootloader how many sectors to read to load the kernel */
-
-  /* check for  --extended option */
-  if(!strncmp(argv[1],"--extended",11)){
-	/* print info */
-  }
-  
-  return 0;
-} // ends main()
-
-
-
